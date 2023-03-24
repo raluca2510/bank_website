49be8670541cf88acd9a3a9d0dd0466acf295590
Raluca Curt <ralucaioanacurt@gmail.com>
2023-03-24 22:46:14 +0200

    feat: structure App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
 import { React } from 'react'
+import GlobalStyle from './styles';
+import styled from 'styled-components'
+
+const AppStyled = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+`
+
+const Container = styled.div`
+  background: var(--bg-main);
+  width: ${props => props.maxBoxWidth ? 'var(--box-width-max)' : '100%'};
+  padding-right: var(--paddingX);
+  padding-left: var(--paddingX);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 function App() {
   return (
-    <div className="App">
+    <AppStyled>
+      <GlobalStyle />
       
-    </div>
+      {/* Navbar */}
+      <Container>
+        <Container maxBoxWidth>
+          
+        </Container>
+      </Container>
+
+      {/* Hero */}
+      <Container>
+        <Container maxBoxWidth>
+
+        </Container>
+      </Container>
+
+      {/* Main */}
+      <Container>
+        <Container maxBoxWidth>
+          
+        </Container>
+      </Container>
+    </AppStyled>
   )
 }
 
