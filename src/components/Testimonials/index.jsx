d5a3e942359cd411f1d219158e3832fbccd1c04c
Raluca Curt <ralucaioanacurt@gmail.com>
2023-03-27 00:08:01 +0300

    feat: implement Testimonials section

---
+++ src/components/Testimonials/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { feedback } from "../../constants";
+import { TestimonialCard } from "../TestimonialCard/index";
+import * as T from './styles'
+
+const Testimonials = () => {
+  return (
+    <T.TestimonialsStyled>
+      <T.Introduction>
+        <T.Heading>What people are saying about us</T.Heading>
+        <T.Container>
+          <T.Description>
+            Everything you need to accept card payments and grow your business
+            anywhere on the planet.
+          </T.Description>
+        </T.Container>
+      </T.Introduction>
+
+      <T.TestimonialsContainer>
+        {feedback.map((card) => (
+          <TestimonialCard key={card.id} {...card} />
+        ))}
+      </T.TestimonialsContainer>
+    </T.TestimonialsStyled>
+  );
+};
+
+export { Testimonials };
---
+++ src/components/Testimonials/styles.jsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const TestimonialsStyled = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: var(--margin-3) 0;
+  gap: var(--margin-4);
+
+  @media (min-width: 768px) {
+    margin: var(--margin-5);
+  }
+`;
+
+export const Introduction = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: var(--paddingY) 0;
+  align-items: center;
+  justify-content: center;
+  gap: var(--margin-2);
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+export const Heading = styled.h2`
+  flex: 1;
+  font-size: var(--h2-font-size);
+  line-height: var(--h2-line-height);
+  color: var(--text-white);
+
+  @media (min-width: 640px) {
+    font-size: var(--h2-font-size-sm);
+    line-height: var(--h2-line-height-sm);
+  }
+`;
+
+export const Container = styled.div`
+  flex: 1;
+  width: 100%;
+`;
+
+export const Description = styled.p`
+  font-size: var(--p-font-size);
+  line-height: var(--p-line-height);
+  text-align: left;
+  max-width: 450px;
+`;
+
+export const TestimonialsContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: var(--margin-2);
+  flex-wrap: wrap;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-around;
+  }
+`;
