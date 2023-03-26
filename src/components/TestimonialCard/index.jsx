537b50a492d71c74f9c84d7ea5811ca23ffa75d8
Raluca Curt <ralucaioanacurt@gmail.com>
2023-03-27 00:01:03 +0300

    feat: implement TestimonialCard

---
+++ src/components/TestimonialCard/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { quotes } from "../../assets";
+import * as T from './styles'
+
+const TestimonialCard = ({ content, name, title, img }) => {
+  return (
+    <T.TestimonialCardStyled>
+      <T.Icon src={quotes} alt="quotes" />
+      <T.Text>{content}</T.Text>
+      <T.Container direction="row" align='center' gap>
+        <T.Picture src={img} alt="profile-picture"></T.Picture>
+        <T.Container direction="column">
+          <T.Name>{name}</T.Name>
+          <T.Job>{title}</T.Job>
+        </T.Container>
+      </T.Container>
+    </T.TestimonialCardStyled>
+  );
+};
+
+export { TestimonialCard };
---
+++ src/components/TestimonialCard/styles.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const TestimonialCardStyled = styled.div`
+  margin: var(--marginY) 0;
+  padding: var(--padding-4) var(--padding-2);
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: var(--margin-3);
+  border-radius: 2rem;
+  min-width: 370px;
+  max-width: 370px;
+
+  &:hover {
+    background: var(--bg-black-gradient);
+  }
+`;
+
+export const Icon = styled.img.attrs((props) => ({
+  src: props.src,
+  alt: props.alt,
+}))`
+  width: 2.6rem;
+  height: 1.7rem;
+  object-fit: contain;
+`;
+
+export const Text = styled.p`
+  font-size: var(--p-font-size);
+  line-height: var(--p-line-height);
+  color: var(--text-white);
+  height: 6.25rem;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: ${(props) => props.direction};
+  gap: ${props => props.gap ? 'var(--margin-sm)' : '0'};
+  align-items: ${props => props.align};
+`;
+
+export const Picture = styled.img.attrs((props) => ({
+  src: props.src,
+  alt: props.alt,
+}))`
+  width: 3rem;
+  height: 3rem;
+  object-fit: contain;
+`;
+
+export const Name = styled.h4`
+  font-size: 1.25rem;
+  line-height: 2rem;
+  color: var(--text-white);
+`;
+
+export const Job = styled.p`
+  line-height: 1.5rem;
+`;
