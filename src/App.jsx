f63057534443323ef41ff34ba2bafc426d733c7c
Raluca Curt <ralucaioanacurt@gmail.com>
2023-03-26 21:40:46 +0300

    feat: add business sections to page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import styled from "styled-components";
 import { Navbar } from "./components/Navbar/index";
 import { Hero } from "./components/Hero/index";
 import Stats from "./components/Stats";
+import { BusinessSection } from "./components/BusinessSection";
+import { navLinks, features } from "./constants";
+import { apple, bill, card, google } from "./assets";
 
 const AppStyled = styled.div`
   width: 100vw;
@@ -18,6 +21,7 @@ const Container = styled.div`
   padding-right: ${(props) => (props.paddingRight ? "var(--padding-3)" : "0")};
   padding-left: ${(props) => (props.paddingLeft ? "var(--padding-3)" : "0")};
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
@@ -47,9 +51,34 @@ function App() {
       </Container>
 
       {/* Main */}
-      <Container>
+      <Container paddingLeft paddingRight>
         <Container maxBoxWidth>
           <Stats />
+          <BusinessSection
+            id={navLinks[1].id}
+            direction='row'
+            heading="You do the business, we’ll handle the money."
+            description="With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market."
+            buttonValue='Get Started'
+            features={features}
+          />
+          <BusinessSection
+            id={navLinks[2].id}
+            direction='row-reverse'
+            heading="Easily control your billing & invoicing"
+            description="Create, send, and track invoices from any device with a wifi connection. Schedule recurring invoices and past-due notices to be sent automatically."
+            img2={apple}
+            img1={google}
+            image={bill}
+          />
+          <BusinessSection
+            id={navLinks[3].id}
+            direction='row'
+            heading="Find a better card deal in few easy steps."
+            description="Easily finance with our credit products or access money from your PayPal balance with our debit card. Shop smarter with a debit card that makes it easier to earn rewards and manage your money."
+            buttonValue='Get Started'
+            image={card}
+          />
         </Container>
       </Container>
     </AppStyled>
