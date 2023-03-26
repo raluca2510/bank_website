4e761488dc41078715af5a8f5e501f7e9718a9bd
Raluca Curt <ralucaioanacurt@gmail.com>
2023-03-26 15:46:23 +0300

    feat: implement Button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ButtonStyled } from "./styles";
+
+const Button = ({ value }) => {
+  return <ButtonStyled>{value}</ButtonStyled>;
+};
+
+export default Button;
---
+++ src/components/Button/styles.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ButtonStyled = styled.button`
+  background: var(--bg-blue-gradient);
+  padding: var(--paddingY) var(--paddingX-sm);
+  font-weight: 500;
+  font-size: 1.2rem;
+  color: var(--bg-main);
+  border-radius: 0.6rem;
+  border: none;
+`;
