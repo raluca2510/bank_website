52222b1ef0c1ac64c0c73ad25484a6f0c0b79287
Raluca Curt <ralucaioanacurt@gmail.com>
2023-03-27 00:28:05 +0300

    feat: implement Clients section

---
+++ src/components/Clients/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { clients } from "../../constants";
+import * as C from './styles'
+
+const Clients = () => {
+  return (
+    <C.ClientsStyled>
+      <C.Container>
+        {clients.map((client) => (
+          <C.Client key={client.id}>
+            <C.Logo src={client.logo} alt="client-logo"></C.Logo>
+          </C.Client>
+        ))}
+      </C.Container>
+    </C.ClientsStyled>
+  );
+};
+
+export { Clients };
---
+++ src/components/Clients/styles.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const ClientsStyled = styled.section`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: var(--margin-3) 0;
+`;
+
+export const Container = styled.div`
+  display: flex;
+//   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100%;
+  gap: var(--margin-2);
+`;
+
+export const Client = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 7.5rem;
+  margin: var(--margin);
+
+  @media (min-width: 768px) {
+    min-width: 12rem;
+  }
+`;
+
+export const Logo = styled.img.attrs((props) => ({
+  src: props.src,
+  alt: props.alt,
+}))`
+  width: 6.25rem;
+  object-fit: contain;
+
+  @media (min-width: 768px) {
+    width: 12rem;
+  }
+`;
